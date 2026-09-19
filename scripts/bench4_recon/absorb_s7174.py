2c6bf2e812e43d61ac6ca0b556dc3262ea2350f1
Z User <z@container>
2026-09-19 14:23:41 +0000

    TASK-332: absorb тул на s7175 (run 35448586519 @ 6dae474)

--- scripts/bench4_recon/absorb_s7174.py
+++ scripts/bench4_recon/absorb_s7174.py
@@ -26,8 +26,8 @@ from collections import Counter
 REPO = "PLANETA9091/c-crussty"
 API = "https://api.github.com"
 RESDIR = "/home/z/c-crussty/research/gc-recon-2026-09-19"
-DEFAULT_RUN = "35447599753"
-EXPECTED_SHA = "463e60b"
+DEFAULT_RUN = "35448586519"
+EXPECTED_SHA = "6dae474"
 
 BASE_LANE_ALLOC = 31.18
 BASE_LANE_CPU = 9.20
