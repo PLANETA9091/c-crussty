b9b769ee62de4caf91e307967fa63a4e071bbc8e
Z User <z@container>
2026-09-23 00:19:11 +0000

    TASK-421-A: purge cargo-target from repo (disk law 66%), gitignore

--- cargo-target/debug/deps/cplug_abi-00c4961ff5849b84.d
+++
@@ -1,7 +0,0 @@
-/home/z/rounds/ROUND-421/agent-a/cargo-target/debug/deps/cplug_abi-00c4961ff5849b84.d: cplug-abi/src/lib.rs
-
-/home/z/rounds/ROUND-421/agent-a/cargo-target/debug/deps/libcplug_abi-00c4961ff5849b84.rlib: cplug-abi/src/lib.rs
-
-/home/z/rounds/ROUND-421/agent-a/cargo-target/debug/deps/libcplug_abi-00c4961ff5849b84.rmeta: cplug-abi/src/lib.rs
-
-cplug-abi/src/lib.rs:
